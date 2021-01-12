fbdc04df18217767c49c50c941ba1d510c5800a3
Vladimir <vladimir.ivinski89@gmail.com>
2021-01-12 03:50:57 +0300

    feat: add correct function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let x = Math.abs(n); // убираем минусы у Number
+    let y = String(x); //Приводим значение к String
+    let result = y.split("").reverse().join("");
+    return result;
+};
